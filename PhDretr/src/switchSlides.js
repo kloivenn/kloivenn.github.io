f6212e6b42e6680150a0e5d6601b7f4fa61056f9
Unknown <svetlana.ovchinnikova@helsinki.fi>
2017-11-22 17:22:27 +0100

    PhD Retreat

---
+++ PhDretr/src/switchSlides.js
@@ -0,0 +1,27 @@
+var slideList = ["index.html", "CombinAssay.html", "slide03.html", "slide04.html", "slide05.html",
+	"slide06.html", "slide07.html", "layers.html",  "slide08.html", "linesAndColour.html", "colourSlider.html",
+	"twoChanel.html", "ASC_AC_withHeatmap.html", "recalculate.html", "slide_last.html"]
+
+document.onkeydown = function(evt){
+	
+	var n = slideList.indexOf(document.location.href.replace(/^.*[\\\/]/, '')),
+		path = document.location.href.match(/^.*[\\\/]/, '');
+	
+	if(document.location.href.replace(/^.*[\\\/]/, '') == "")
+		n = 0;
+
+	if(n == -1)
+		throw "Error: something is wrong! :)";
+
+	if(evt.keyCode == 37){
+		//go to the previous slide
+		if(n > 0)
+			window.open(path + slideList[n - 1],"_self");
+	}
+
+	if(evt.keyCode == 39 || evt.keyCode == 32){
+		//go to the next slide
+		if(n < slideList.length - 1)
+			window.open(path + slideList[n + 1], "_self");
+	}
+}
